07d28f3cc81f49acc47d60157139ccb12f2edf51
Valery Semenenko <gearmobile@gmail.com>
2025-07-15 14:12:57 +0300

    withEntity

--- src/app/pages/book-presenter/store/book-presenter.store.ts
+++ src/app/pages/book-presenter/store/book-presenter.store.ts
@@ -1,17 +1,35 @@
 import { computed } from '@angular/core';
-import { patchState, signalMethod, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { patchState, signalMethod, signalStore, type, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
 import { withDevtools } from '@angular-architects/ngrx-toolkit';
 
 import { initialBookPresenterSlice } from './book-presenter.slice';
 import { BOOKS_COLLECTION } from '../../../data/books-collection';
+import { entityConfig, setAllEntities, updateEntity, withEntities } from '@ngrx/signals/entities';
+import { Book } from '../../../models/book.model';
+
+const bookConfig = entityConfig({
+    entity: type<Book>(),
+    collection: '_books',
+    selectId: (book: Book) => book.id
+});
 
 export const BookPresenterStore = signalStore(
     withState(initialBookPresenterSlice),
+    withDevtools('books-browser'),
+    withEntities(bookConfig),
     withComputed(store => ({
-        book: computed(() => BOOKS_COLLECTION[store.id()])
+        book: computed(() => store._booksEntityMap()[store.id()])
     })),
     withMethods(store => ({
-        setBookId: signalMethod<number>(id => patchState(store, { id }))
+        setBookId: signalMethod<number>(id => patchState(store, { id })),
+        renameBook: (name: string) => patchState(store, updateEntity({
+            id: store.id(),
+            changes: { title: name }
+        }, bookConfig))
     })),
-    withDevtools('books-browser')
+    withHooks(store => ({
+        onInit: () => {
+            patchState(store, setAllEntities(BOOKS_COLLECTION, bookConfig))
+        }
+    }))
 )
